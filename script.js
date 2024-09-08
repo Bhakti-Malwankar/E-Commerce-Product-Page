41a3bd81ba7c9830ba0b7498d29ac484eaeb9950
Bhakti-Malwankar <166507795+Bhakti-Malwankar@users.noreply.github.com>
2024-09-08 21:58:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let slide = document.querySelectorAll(".slideCard");
+let cards = document.querySelectorAll(".card");
+let count = 0;
+
+slide.forEach(function(slides, index){
+    slides.style.left=`${index * 100}%`
+})
+
+function myFun(){
+    slide.forEach(function(curVal){
+        curVal.style.transform=`translateX(-${count * 100}%)`
+    })
+}
+
+setInterval(function(){
+    count++
+    if(count == slide.length){
+        count=0;
+    }
+    myFun();
+}, 2000);
+
+// show card details
+cards.forEach(function(curCard){
+    curCard.addEventListener("click", function(){
+        console.log(curCard.firstElementChild.src);
+
+        document.querySelector(".container").style.display="none"
+        document.querySelector("nav").style.display="flex"
+
+        let div = document.createElement("div");
+        div.classList.add("cardDetail");
+        div.innerHTML=`
+        <img src=${curCard.firstElementChild.src} alt="">
+        <div class="detailText">
+            <h2>Top trends</h2>
+            <h3>30% OFF on Trends Wear</h3>
+            <p>Product Details:</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+            <p>Vel dignissimos facere adipisci suscipit repel.</p>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star"></i>
+            <i class="fa-solid fa-star-half-stroke"></i><br/>
+            <button>Buy Now</button>
+            <button>Add To Cart</button>
+            <a href="">Back</a>
+
+        </div>
+        `
+        document.querySelector("body").appendChild(div)
+
+        // document.querySelector(".container").style.display="none";
+
+    })
+})
+
+
+    
